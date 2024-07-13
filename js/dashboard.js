d4a0987991c88ee00d864a44af5b2c0f8d8232b8
kavishnnx2 <kavish.mojhoa@nx2square.com>
2024-07-14 00:18:27 +0400

    lgout

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,3 +2,9 @@ const isLoggedIn = localStorage.getItem('isLoggedIn');
 if (!isLoggedIn) {
     window.location.href = 'login.html'; // Redirect to login page if not logged in
 }
+const logoutLink = document.getElementById('logoutLink');
+logoutLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    localStorage.removeItem('isLoggedIn'); // Clear session
+    window.location.href = 'index.html'; // Redirect to login page
+});
